08893fe3cb7381c8997577fe9183eb57af05c33a
Ginevra Testa <ginevra99testaa@gmail.com>
2023-11-01 12:07:13 +0100

    work in progress

--- script.js
+++ script.js
@@ -1,90 +1,4 @@
-// NON MODIFICARE QUESTO ARRAY!
-const jobs = [
-  { title: "Marketing Intern", location: "US, NY, New York" },
-  {
-    title: "Customer Service - Cloud Video Production",
-    location: "NZ, Auckland",
-  },
-  {
-    title: "Commissioning Machinery Assistant (CMA)",
-    location: "US, IA, Wever",
-  },
-  {
-    title: "Account Executive - Washington DC",
-    location: "US, DC, Washington",
-  },
-  { title: "Bill Review Manager", location: "US, FL, Fort Worth" },
-  { title: "Accounting Clerk", location: "US, MD," },
-  { title: "Head of Content (m/f)", location: "DE, BE, Berlin" },
-  {
-    title: "Lead Guest Service Specialist",
-    location: "US, CA, San Francisco",
-  },
-  { title: "HP BSM SME", location: "US, FL, Pensacola" },
-  {
-    title: "Customer Service Associate - Part Time",
-    location: "US, AZ, Phoenix",
-  },
-  {
-    title: "ASP.net Developer Job opportunity at United States,New Jersey",
-    location: "US, NJ, Jersey City",
-  },
-  {
-    title: "Talent Sourcer (6 months fixed-term contract)",
-    location: "GB, LND, London",
-  },
-  {
-    title: "Applications Developer, Digital",
-    location: "US, CT, Stamford",
-  },
-  { title: "Installers", location: "US, FL, Orlando" },
-  { title: "Account Executive - Sydney", location: "AU, NSW, Sydney" },
-  {
-    title: "VP of Sales - Vault Dragon",
-    location: "SG, 01, Singapore",
-  },
-  { title: "Hands-On QA Leader", location: "IL, Tel Aviv, Israel" },
-  {
-    title: "Southend-on-Sea Traineeships Under NAS 16-18 Year Olds Only",
-    location: "GB, SOS, Southend-on-Sea",
-  },
-  { title: "Visual Designer", location: "US, NY, New York" },
-  {
-    title: "Process Controls Engineer - DCS PLC MS Office - PA",
-    location: "US, PA, USA Northeast",
-  },
-  { title: "Marketing Assistant", location: "US, TX, Austin" },
-  { title: "Front End Developer", location: "NZ, N, Auckland" },
-  { title: "Engagement Manager", location: "AE," },
-  {
-    title: "Vice President, Sales and Sponsorship (Businessfriend.com)",
-    location: "US, CA, Carlsbad",
-  },
-  { title: "Customer Service", location: "GB, LND, London" },
-  { title: "H1B SPONSOR FOR L1/L2/OPT", location: "US, NY, New York" },
-  { title: "Marketing Exec", location: "SG," },
-  {
-    title: "HAAD/DHA Licensed Doctors Opening in UAE",
-    location: "AE, AZ, Abudhabi",
-  },
-  {
-    title: "Talent Management Process Manager",
-    location: "US, MO, St. Louis",
-  },
-  { title: "Customer Service Associate", location: "CA, ON, Toronto" },
-  {
-    title: "Customer Service Technical Specialist",
-    location: "US, MA, Waltham",
-  },
-  { title: "Software Applications Specialist", location: "US, KS," },
-  { title: "Craftsman Associate", location: "US, WA, Everett" },
-  { title: "Completion Engineer", location: "US, CA, San Ramon" },
-  { title: "I Want To Work At Karmarama", location: "GB, LND," },
-  {
-    title: "English Teacher Abroad",
-    location: "US, NY, Saint Bonaventure",
-  },
-];
+import {jobs} from './data'
 /* PARTE 1: 
 Oggi analizzeremo un problema molto comune: realizzare algoritmi di ricerca.
 Il tuo compito è creare una funzione che cercherà per posizione lavorativa E posizione geografica. Questi due valori verranno passati come parametri
@@ -159,8 +73,9 @@ Dopo aver raccolto ed elaborato i dati, e’ il momento di mostrare i risultati 
 
 */
 
-// Dichiariamo la costante list che rappresenta l'elemento HTML <ul>
+// Dichiariamo la costante list che rappresenta l'elemento HTML <ul> e counter che rappresenta <p>
 const list = document.querySelector("#resultsList");
+const counter = document.querySelector("#resultsCounter")
 
 /* Definiamo una funzione chiamata searchDreamJob
    e facciamo sì che si esegua ogni volta che l'utente farà click sul pulsante SEARCH.
@@ -172,10 +87,11 @@ function searchDreamJob() {
   const jobLocationInput = document.querySelector("#jobLocation");
   const title = jobTitleInput.value.toLowerCase();
   const location = jobLocationInput.value.toLowerCase();
+  let resultCount = 0;
 
   /* Puliamo la lista dei risultati in modo che non si mostri la lista per intero */
   list.innerHTML = "";
-
+  counter.innerText= "";
   /* Per ciascun elemento dell'array, verifichiamo se il titolo e la posizione del lavoro corrente
     includono i valori title e location ottenuti dagli input convertiti in lettere minuscole. 
     Se c'è una corrispondenza, creiamo un elemento di lista <li> e lo aggiungiamo all'elemento list. 
@@ -185,8 +101,10 @@ function searchDreamJob() {
       jobs[i].title.toLowerCase().includes(title) &&
       jobs[i].location.toLowerCase().includes(location)
     ) {
+      resultCount++;
       list.innerHTML += `<li>${jobs[i].title} ${jobs[i].location}</li>`;
     }
+    counter.innerText = `Results found: ${resultCount}`;
   }
 
   /* Se non vengono trovati risultati che soddisfano i criteri di ricerca, 
@@ -195,9 +113,11 @@ function searchDreamJob() {
      e tramite alert mostriamo un messaggio di spiegazioni.*/
   if (title.trim() === "" || location.trim() === "") {
     list.innerHTML = "<li>No Job found.</li>";
+    counter.innerText = `No results.`;
     alert("Insert both Title and Location for a better research.");
   }
 }
+
 
 /* Se l'utente fa clic sul pulsante "SEARCH" l'evento click attiva la funzione searchDreamJob. */
 const searchButton = document.querySelector("#searchButton");
